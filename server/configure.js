e7dcb22fd829b8011a481cc44dd7f494e47ac2ab
Akash Nidhi P S <akash.nidhi@interactionone.com>
2016-09-08 19:53:09 +0530

    Created dummy data and binded it to UI

--- server/configure.js
+++ server/configure.js
@@ -7,6 +7,9 @@ var path=require('path'),
 	morgan=require('morgan'),
 	methodOverride=require('method-override'),
 	moment=require('moment'),
+	multer = require('multer'),
+	
+
 	errorHandler=require('errorhandler');
 
 module.exports=	function(app){
@@ -18,6 +21,21 @@ module.exports=	function(app){
 	routes(app);//moving the routes to routes folder.
 	app.use('/public/', express.static(path.join(__dirname,  
            '../public')));
+	
+
+
+	//upload=multer.diskStorat({ dest: path.join(__dirname,'../public/upload/temp')}),
+
+	var storage = multer.diskStorage({
+  		destination: "../public/upload/temp",
+		filename: function (req, file, cb) {
+		    cb(null, file.fieldname + '-' + Date.now())
+		  }
+	});
+
+	var upload = multer({ storage: storage });
+
+	app.use(upload.single('img'));
  
 
 	if ('development' === app.get('env')) {
